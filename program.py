27e199770d8bc4fbd8eb941d939f5fce12e5882e
3rick30090 <erickbu2007@gmail.com>
2025-08-29 11:55:45 -0600

    Add files via upload
    Avance 2 (solo operaciones que se hacer hasta el momento)

---
+++ program.py
@@ -0,0 +1,20 @@
+print('Prueba de operaciones en lo que aprendo if')
+print('esta organizada como:')
+print('Division')
+print('Multiplicacion')
+print('Suma')
+print('Resta')
+print('Porcentaje')
+print('pon dos numeros para calcular')
+a = float(input())
+b = float(input())
+X = a / b
+print("%.3f" % (X))
+X = a * b
+print(int(X))
+X = a + b 
+print("%.3f" % (X))
+X = a - b 
+print("%.3f" % (X))
+X = a * (b/100)
+print("%.3f" % (X))
