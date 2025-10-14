2ae3f26ebad40d3e80bdf29649074713eeb98a29
3rick30090 <erickbu2007@gmail.com>
2025-10-14 11:55:58 -0600

    proyecto.py

--- Avance_7.py
+++ Avance_7.py
@@ -1,25 +1,72 @@
+"""
+Proyecto Calculadora 
+El proyecto trata de simular como una calculadora 
+y darle al usuario lo que quiera que este a su alcance usando operadores basicos
+o funciones más avanzadas
+"""
+#bibliotecas
 import random
+"""
+esta bibloteca la use como especie de dado entre numeros que pida el usuario
+"""
 import math
-
+"""
+esta bibloteca la use para poder usar las potencias y las raices de un numero que guste el usuario 
+"""
+"""
+operadores basicos
+"""
 def suma(a, b):
+    """
+    uso de operadores y funciones (a, b) 
+    pide a valor númerico  y b valor númerico
+    el programa suma a y b y devuelve el resultado de dicha operacion
+    """
     return a + b
 
 def resta(a, b):
+        """
+    uso de operadores y funciones (a, b) 
+    pide a valor númerico  y b valor númerico
+    el programa resta a y b y devuelve el resultado de dicha operacion
+    """
     return a - b
 
 def multiplicacion(a, b):
+        """
+    uso de operadores y funciones (a, b) 
+    pide a valor númerico  y b valor númerico
+    el programa multiplica a y b y devuelve el resultado de dicha operacion
+    """
     return a * b
 
 def division(a, b):
+        """
+    uso de operadores, funciones y condicional (a, b) 
+    pide a valor númerico  y b valor númerico
+    el programa y a y b y devuelve el resultado de dicha operacion y si termina una division
+    matematicamente imposible devuelve error 
+    """
     if b != 0:
         return a / b
     else:
         return "Error, no se puede dividir entre 0"
 
 def porcentaje(a, b):
+        """
+    uso de operadores y funciones (a, b) 
+    pide a valor númerico  y b valor númerico
+    el programa usa a como el valor principal y b como el porcentaje y devuelve el resultado de dicha operacion
+    """
     return a * (b / 100)
 
 def factorial(n):
+        """
+    uso de operadores, funciones, condicional y ciclos (n) 
+    pide a valor númerico 
+    el programa usa n como el numero con el factorial, si el usuario pide numeros negativos marcara error al no ser posible y utilizando ciclos
+    va restando su mismo numero para ir multiplicando
+    """
     if n < 0:
         return "Error, no existe factorial de números negativos"
     resultado = 1
@@ -29,6 +76,11 @@ def factorial(n):
     return resultado
 
 def leer_matriz():
+        """
+    uso de funciones, listas, listas anadinas, ciclos y ciclos anidados
+    pide n valor de escala y pide i y j como valores de escala y numerico pidiendo al usuario escribir su propia matriz
+    el programa con los datos dados crea la matriz y la guarda para que el usuario la pueda sumar o multiplicar 
+    """
     n = int(input("Elige el tamaño: 2 para 2x2 o 3 para 3x3: "))
     print("Ingresa los valores de la matriz:")
     matriz = []
@@ -41,6 +93,12 @@ def leer_matriz():
     return matriz
 
 def suma_mat(a, b):
+        """
+    uso de operadores, funciones, listas, listas anidadas, ciclos ,ciclos anidadas
+    usando la matriz que fue solicitada en la anterior funcion suma la matriz de fila a columna (i, j) y más adelante si el tamaño es distinto 
+    marcara error al no ser posible 
+    usando n de la anterior funcion para ver la matriz guardada
+    """
     n = len(a)
     c = [[0 for _ in range(n)] for _ in range(n)]
     for i in range(n):
@@ -49,6 +107,11 @@ def suma_mat(a, b):
     return c
 
 def mult_mat(a, b):
+    """
+        uso de operadores, funciones, listas, listas anidadas, ciclos ,ciclos anidadas
+    usando la matriz que fue solicitada en la anterior funcion multiplica la matriz de fila a columna (i, j) 
+    usando n de la anterior funcion para ver la matriz guardada
+"""
     n = len(a)
     c = [[0 for _ in range(n)] for _ in range(n)]
     for i in range(n):
@@ -57,10 +120,19 @@ def mult_mat(a, b):
     return c
 
 def imprimir_matriz(matriz):
+    """
+    uso de funciones, listas, listas anidadas, ciclos, ciclos anidadas
+    despues de que la matriz dada y calculada esta funcion imprime el resultado de dicha matriz si es posible 
+    """
     for fila in matriz:
         print(fila)
 
 def num_random():
+    """
+    uso de funciones, listas, listas anidadas, ciclos, ciclos anadidos y biblotecas
+    recibe n como el total de numeros en la lista y valor para el tener esos numeros aleatorios o no que de el usuario para devolver 
+    en esa lista un numero al azar 
+    """
     n = int(input("¿Cuántos números tendrá la lista?: "))
     lista = []
 
@@ -72,9 +144,20 @@ def num_random():
     print("Número aleatorio elegido de la lista:", lista[indice])
 
 def potencia(a, b):
+    """
+    uso de operadores y biblotecas
+    recibe a como el numero y b como la potencia 
+    devuelve la potencia de dicho numero 
+    """
     return pow(a, b)
 
 def raiz_cuadrada(x):
+  """
+  usa operadores, condicionales y biblotecas 
+  recibe x como el numero que quieren sacar la raiz 
+  y si ese numero llega a ser negativo devuelve error
+  si no devuelve la raiz de dicho numero
+  """
     if x < 0:
         return "Error: no se puede calcular raíz de número negativo"
     return math.sqrt(x)
@@ -94,7 +177,14 @@ print("10. Potencia")
 print("11. Raices")
 print("\n")
 opcion = input("Elige una opción: ")
-
+"""
+Lista de opciones para elegir
+"""
+"""
+usando condicional dependiendo de la opcion elegida usa una funcion y con una lista guarda los operadores basicos [1 , 2 , 3, 4 , 5] que usan 
+solo 2 variables (a, b) para facilitar y no andar poniendo variables a cada funcion 
+y si es distinto a los operadores basico usa otras funciones y operadores y si pone otro numero que no este marca invalido 
+"""
 if opcion in ["1", "2", "3", "4", "5"]:
     a = int(input("Número 1: "))
     b = int(input("Número 2: "))
@@ -106,13 +196,16 @@ if opcion in ["1", "2", "3", "4", "5"]:
     elif opcion == "3":
         print("Resultado de la multiplicación:", multiplicacion(a, b))
     elif opcion == "4":
+        """
+        con ciclos si el usuario como al dividendo como 0 lo devolvera la opcion para que cambie de numero 
+        y seguira asi hasta que el 0 no este en la variable
+        """
         while b == 0:
             print("No se puede dividir entre 0, intenta de nuevo.")
             b = int(input("Número 2: "))
         print("Resultado de la división:", division(a, b))
     elif opcion == "5":
         print("Resultado del porcentaje:", porcentaje(a, b))
-
 elif opcion == "6":
     n = int(input("Introduce un número entero para calcular su factorial: "))
     print("Factorial:", factorial(n))
@@ -120,7 +213,10 @@ elif opcion == "6":
 elif opcion in ["7", "8"]:
     mat1 = leer_matriz()
     mat2 = leer_matriz()
-
+ """
+ con condicional se usa como limite para las matrizes porque si la matriz es distinta de tamaño
+ no podra sumar
+ """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         print("No es posible operar con matrices de diferente tamaño")
     else:
@@ -141,4 +237,5 @@ elif opcion == "11":
     x = int(input("¿Cúal número quiere conocer su raiz?"))
     print("La raiz es:", raiz_cuadrada(x))
 else:
+
     print("Opción no válida.")
